8306d3fa745d75ebab20450d6bc77557a870e269
karimamaaoui <67017326+karimamaaoui@users.noreply.github.com>
2021-01-19 22:25:41 +0100

    Update list.component.ts

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 import { meeting } from './meeting';
 
 @Component({
@@ -24,4 +24,42 @@ export class ListComponent implements OnInit {
   }
   
 
+}
+*/
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MeetingServService } from '../meeting-serv.service';
+import { Meeting } from './meeting';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  meetings: Meeting[] = [];
+
+  constructor(private meetingService: MeetingServService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.meetings = this.meetingService.getMeetings();
+  }
+
+  showEditMeeting(meeting: Meeting): void {
+    this.router.navigateByUrl('edit/' + meeting.idMeet);
+  }
+
+  showAddMeeting(): void {
+    this.router.navigateByUrl('add');
+  }
+
+  deleteMeeting(indice : number){
+    this.meetingService.deleteMeeting(indice);
+    this.meetingService.saveMeeting();
+  }
+  
+  showDetails(meeting:Meeting):void{
+    alert("Id Meeting : "+meeting.idMeet+" \n le titre est "+meeting.titre+"\n Date Debut "+meeting.dateDeb+"\n Date Fin "+meeting.dateFin);
+    //this.router.navigateByUrl('details/'+meeting.idMeet);
+  }
 }
