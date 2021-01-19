47c1a65bfacd77d1cfe7386413795a28a3e19f8d
karimamaaoui <67017326+karimamaaoui@users.noreply.github.com>
2021-01-19 22:35:51 +0100

    Update meeting.ts

--- src/app/list/meeting.ts
+++ src/app/list/meeting.ts
@@ -1,20 +1,6 @@
-export class meeting {
+export interface Meeting {
     idMeet:number;
     titre:string;
     lieu:string;
-    dateDeb:Date;
-    dateFin:Date;
-
-
-
-    constructor (idm:number,t:string,l:string,dd:Date,df:Date)
-    {
-        this.idMeet=idm;
-        this.titre=t;
-        this.lieu=l;
-        this.dateDeb=dd;
-        this.dateFin=df;
-    }
-    
-
-}
+    dateDeb:string;
+    dateFin:string;}
